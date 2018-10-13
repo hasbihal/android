e44b33f740f4082717860e6e145b559360387f01
Uğur Çakmak <ugurcmk@gmail.com>
2018-10-14 00:10:35 +0300

    Update main item ui

--- app/src/main/java/com/hasbihal/extension/ImageView.kt
+++ app/src/main/java/com/hasbihal/extension/ImageView.kt
@@ -2,9 +2,17 @@ package com.hasbihal.extension
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 fun ImageView.load(url: String){
     Glide.with(this.context)
             .load(url)
+            .into(this)
+}
+
+fun ImageView.loadWithRounded(url: String){
+    Glide.with(this.context)
+            .load(url)
+            .apply(RequestOptions.circleCropTransform())
             .into(this)
 }
