eebf6efdffaea69241d1757cdc5e1493de99352a
Uğur Çakmak <ugurcmk@gmail.com>
2018-10-15 23:54:12 +0300

    Add main adapter

---
+++ app/src/main/java/com/hasbihal/main/MainAdapter.kt
@@ -0,0 +1,34 @@
+package com.hasbihal.main
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hasbihal.extension.load
+import kotlinx.android.synthetic.main.main_item.view.*
+
+class MainAdapter(private val context: Context,
+                  private val data: List<User>,
+                  private val layoutID: Int) : RecyclerView.Adapter<MainViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        return MainViewHolder(LayoutInflater.from(context).inflate(layoutID, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+       return data.size
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.txtDescription.text = data[position].descripton
+        holder.imgProfile.load(data[position].photoUrl)
+    }
+
+
+}
+
+class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val imgProfile = view.imgProfile
+    val txtDescription = view.txtDescription
+}
