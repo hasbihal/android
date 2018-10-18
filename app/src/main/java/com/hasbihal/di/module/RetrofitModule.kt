67fab4c55d7184ef63c407f40f8e7932fac730a0
Uğur Çakmak <ugurcmk@gmail.com>
2018-10-18 14:57:55 +0300

    Add custom okhttpclient instance

--- app/src/main/java/com/hasbihal/di/module/RetrofitModule.kt
+++ app/src/main/java/com/hasbihal/di/module/RetrofitModule.kt
@@ -1,15 +1,30 @@
 package com.hasbihal.di.module
 
+import com.hasbihal.data.repository.IRepository
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val RetrofitModule = module {
 
-    single {
-        //createWebService(okHttpClient, url)
-    }
+    single { createOkHttpClient() }
+
+    single { createWebService<IRepository>(get(), getProperty(BASE_URL)) }
+}
+
+const val BASE_URL = "BASE URL"
+
+fun createOkHttpClient(): OkHttpClient {
+    val httpLoggingInterceptor = HttpLoggingInterceptor()
+    httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+    return OkHttpClient.Builder()
+            .connectTimeout(60L, TimeUnit.SECONDS)
+            .writeTimeout(60L, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
 }
 
 inline fun <reified T> createWebService(okHttpClient: OkHttpClient, url: String): T {
