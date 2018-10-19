c94fc92e7d4ba10ffe90d91a6815457586ad2038
Uğur Çakmak <ugurcmk@gmail.com>
2018-10-19 11:16:47 +0300

    Add extension which determine whether network is exist

--- app/src/main/java/com/hasbihal/extension/Context.kt
+++ app/src/main/java/com/hasbihal/extension/Context.kt
@@ -1,2 +1,10 @@
 package com.hasbihal.extension
 
+import android.content.Context
+import android.net.ConnectivityManager
+
+fun Context.isNetworkConnected(): Boolean {
+    val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val networkInfo = cm.activeNetworkInfo
+    return networkInfo != null && networkInfo.isConnected
+}
