0a0042deea86ce37ac26cb5bd2b9f886f841169f
Uğur Çakmak <ugurcmk@gmail.com>
2018-10-22 14:46:32 +0300

    Implement main databse class

--- app/src/main/java/com/hasbihal/data/local/HasbihalDatabase.kt
+++ app/src/main/java/com/hasbihal/data/local/HasbihalDatabase.kt
@@ -1,9 +1,31 @@
 package com.hasbihal.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.hasbihal.data.entity.Message
 
-//@Database()
+@Database(entities = [Message::class], version = 1)
 abstract class HasbihalDatabase: RoomDatabase() {
+    companion object {
 
+        private var INSTANCE: HasbihalDatabase? = null
+        private val lock = Any()
+
+        fun getInstance(context: Context): HasbihalDatabase {
+            synchronized(lock){
+                if (INSTANCE == null){
+                    INSTANCE = Room
+                            .databaseBuilder(context.applicationContext,
+                                             HasbihalDatabase::class.java,
+                                      "HDatabase.db")
+                            .build()
+                }
+
+                return INSTANCE!!
+            }
+        }
+
+    }
 }
