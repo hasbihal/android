e71ef9aac5b48ed709afd979ddc650a888780511
Uğur Çakmak <ugurcmk@gmail.com>
2018-10-29 18:09:14 +0300

    Separate the repository and data source

--- app/src/main/java/com/hasbihal/data/repository/UserRepository.kt
+++ app/src/main/java/com/hasbihal/data/repository/UserRepository.kt
@@ -3,8 +3,9 @@ package com.hasbihal.data.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.hasbihal.data.model.UserEntity
+import com.hasbihal.data.remote.RemoteDataSource
 
-class UserRepository{
+class UserRepository(val dataSource: RemoteDataSource){
 
     fun getData(): MutableLiveData<List<UserEntity>> {
         return MutableLiveData<List<UserEntity>>()
