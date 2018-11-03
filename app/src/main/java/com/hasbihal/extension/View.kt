f807cc38791063066aa045e1f205e806b5d64684
Uğur Çakmak <ugurcmk@gmail.com>
2018-11-03 23:27:32 +0300

    Add extesion function for Snackbar

--- app/src/main/java/com/hasbihal/extension/View.kt
+++ app/src/main/java/com/hasbihal/extension/View.kt
@@ -3,6 +3,7 @@ package com.hasbihal.extension
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import com.google.android.material.snackbar.Snackbar
 
 fun View.showKeyboard(){
     val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -12,4 +13,8 @@ fun View.showKeyboard(){
 fun View.hideKeyboard(){
     val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun View.showSnackbar(snackbarText: String, timeLength: Int) {
+    Snackbar.make(this, snackbarText, timeLength).show()
 }
