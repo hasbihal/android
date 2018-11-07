746a8aac9dcc54f3a9bea8e45506db3e073a8bd2
Uğur Çakmak <ugurcmk@gmail.com>
2018-11-08 00:02:37 +0300

    Change DI graph

--- app/src/main/java/com/hasbihal/di/modules.kt
+++ app/src/main/java/com/hasbihal/di/modules.kt
@@ -1,58 +1,26 @@
-package com.hasbihal.di.module
+package com.hasbihal.di
 
 import com.hasbihal.BASE_URL
 import com.hasbihal.BuildConfig
-import com.hasbihal.data.IDataSource
-import com.hasbihal.data.remote.RemoteDataSource
 import com.hasbihal.data.repository.UserRepository
 import com.hasbihal.main.UserViewModel
 import com.hasbihal.network.MessageApi
 import com.hasbihal.network.UserApi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import org.koin.androidx.viewmodel.experimental.builder.viewModel
+import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.Module
 import org.koin.dsl.module.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-val RetrofitModule: Module = module {
-
-    // Create Retrofit instance
-    single { createOkHttpClient() }
-    single { createWebService( get() ) }
-
-    // Create end-points
-    single{ provideUserApi(get()) }
-    single{ provideMessagesApi(get()) }
-}
-
-val UserViewModelModule : Module = module {
-
-    single<IDataSource> { RemoteDataSource() }
-
-    single { UserRepository( get() ) }
-
-    viewModel<UserViewModel>()
-}
-
 val viewModelModule: Module = module {
-    viewModel { PostListViewModel(usersPostsUseCase = get()) }
-    viewModel { PostDetailsViewModel(userPostUseCase = get(), commentsUseCase = get()) }
-}
-
-val useCaseModule: Module = module {
-    factory { UsersPostsUseCase(userRepository = get(), postRepository = get()) }
-    factory { UserPostUseCase(userRepository = get(), postRepository = get()) }
-    factory { CommentsUseCase(commentRepository = get()) }
+    viewModel { UserViewModel(get()) }
 }
 
 val repositoryModule: Module = module {
     single { UserRepository(get()) }
-    single { UserRepositoryImpl(userApi = get(), cache = get(USER_ENTITY_CACHE)) as UserRepository }
-    single { PostRepositoryImpl(postsApi = get(), cache = get(POST_ENTITY_CACHE)) as PostRepository }
-    single { CommentRepositoryImpl(commentsApi = get(), cache = get(COMMENT_ENTITY_CACHE)) as CommentRepository }
 }
 
 val networkModule: Module = module {
@@ -65,13 +33,6 @@ private val retrofit: Retrofit = createWebService(createOkHttpClient(BuildConfig
 private val userApi: UserApi = retrofit.create(UserApi::class.java)
 private val messageApi: MessageApi = retrofit.create(MessageApi::class.java)
 
-
-fun provideUserApi(retrofit: Retrofit): UserApi {
-    return retrofit.create(UserApi::class.java)
-}
-fun provideMessagesApi(retrofit: Retrofit): MessageApi {
-    return retrofit.create(MessageApi::class.java)
-}
 
 private fun createOkHttpClient(debug: Boolean): OkHttpClient {
     val httpLoggingInterceptor = HttpLoggingInterceptor()
