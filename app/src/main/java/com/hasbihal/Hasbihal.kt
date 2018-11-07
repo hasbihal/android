9437b6157773c105bf3b48eefe2f03c9e9dfa514
Uğur Çakmak <ugurcmk@gmail.com>
2018-11-08 00:04:00 +0300

    DI classes add to App class

--- app/src/main/java/com/hasbihal/Hasbihal.kt
+++ app/src/main/java/com/hasbihal/Hasbihal.kt
@@ -3,6 +3,9 @@ package com.hasbihal
 import android.app.Application
 import com.hasbihal.di.module.RetrofitModule
 import com.hasbihal.di.module.UserViewModelModule
+import com.hasbihal.di.networkModule
+import com.hasbihal.di.repositoryModule
+import com.hasbihal.di.viewModelModule
 import org.koin.android.ext.android.startKoin
 import timber.log.Timber
 
@@ -14,8 +17,9 @@ class Hasbihal: Application() {
         super.onCreate()
 
         startKoin(this, listOf(
-                UserViewModelModule,
-                RetrofitModule))
+                viewModelModule,
+                repositoryModule,
+                networkModule))
 
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
