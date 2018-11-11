f33b260bde71895814125286593921ddff9e6ead
Uğur Çakmak <ugurcmk@gmail.com>
2018-11-11 23:22:38 +0300

    Add add and delete methods into the MessageDao

--- app/src/main/java/com/hasbihal/data/local/dao/MessageDao.kt
+++ app/src/main/java/com/hasbihal/data/local/dao/MessageDao.kt
@@ -1,13 +1,23 @@
 package com.hasbihal.data.local.dao
 
+import android.content.ClipData
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
 import com.hasbihal.data.entity.Message
 
 @Dao
 interface MessageDao {
 
+    // This should be reconfigured
     @Query("SELECT * FROM Message WHERE sender = id ORDER BY date DESC")
     fun all(id: Int): LiveData<List<Message>>
+
+    @Insert
+    fun add(vararg message: Message)
+
+    @Delete
+    fun delete(vararg message: Message)
 }
