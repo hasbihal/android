f30863c0d7d7f56e2ef6dc2e367803f0e40bf1c2
Uğur Çakmak <ugurcmk@gmail.com>
2018-11-13 23:07:11 +0300

    Add dummy user data

--- app/src/main/java/com/hasbihal/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/hasbihal/data/remote/RemoteDataSource.kt
@@ -1,10 +1,5 @@
 package com.hasbihal.data.remote
 
-import com.hasbihal.data.IDataSource
-import com.hasbihal.network.UserApi
-import okhttp3.internal.Internal.instance
-import retrofit2.Retrofit
-
 class RemoteDataSource{
 
 }
