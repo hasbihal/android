946d8e7794ededdd04aec5ff8194c72b23e3a1c5
Uğur Çakmak <ugurcmk@gmail.com>
2018-11-22 23:37:54 +0300

    Add application context

--- app/src/main/java/com/hasbihal/Hasbihal.kt
+++ app/src/main/java/com/hasbihal/Hasbihal.kt
@@ -1,13 +1,23 @@
 package com.hasbihal
 
 import android.app.Application
+import android.content.Context
+import org.koin.android.ext.android.get
 import org.koin.android.ext.android.startKoin
 import timber.log.Timber
 
 class Hasbihal: Application() {
 
+    companion object{
+        var app: Application? = null
+    }
+
     override fun onCreate() {
         super.onCreate()
+
+        if (app == null){
+            app = this
+        }
 
         startKoin(this, listOf(
                 viewModelModule,
