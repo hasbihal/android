68930909582cdc19c4d953aea6c0e4da72d648b2
Uğur Çakmak <ugurcmk@gmail.com>
2018-12-02 22:55:13 +0300

    Fix fake repository

--- app/src/apiTesting/java/com/hasbihal/FakeUserRepository.kt
+++ app/src/apiTesting/java/com/hasbihal/FakeUserRepository.kt
@@ -3,30 +3,19 @@ package com.hasbihal
 import android.content.Context
 import com.hasbihal.data.repository.Repository
 import com.hasbihal.data.response.Response
+import com.hasbihal.data.response.ResponseType
 
 class FakeUserRepository(val context: Context) : Repository{
 
-/*
-*
-*
-* {
-      "avatar": null,
-      "email": "asfas@gmail.com",
-      "gender": "both",
-      "id": 23,
-      "location": "istanbul",
-      "name": "asfas",
-      "summary": "developer"
-    }
-*
-*
-*
-* */
-
+    private val RESPONSE_CODE = 0
 
     override fun getData(): Response {
         val usersJsonObject = loadJsonFile(context, "users.json")
 
-        return Response
+        return Response(
+                code = RESPONSE_CODE,
+                message = "successful",
+                isSuccessful = ResponseType.SUCCESS,
+                body = usersJsonObject)
     }
 }
