292c59819b5d38a95d5249b4deaf15809fed3c1a
Uğur Çakmak <ugurcmk@gmail.com>
2018-12-09 22:34:02 +0300

    Fix the parsing users' data

--- app/src/apiTesting/java/com/hasbihal/FakeUserRepository.kt
+++ app/src/apiTesting/java/com/hasbihal/FakeUserRepository.kt
@@ -6,15 +6,16 @@ import com.google.gson.reflect.TypeToken
 import com.hasbihal.data.model.User
 import com.hasbihal.data.repository.Repository
 import com.hasbihal.data.response.Response
+import java.lang.reflect.Type
 
 class FakeUserRepository(val context: Context) : Repository{
 
     private val RESPONSE_CODE = 0
 
     override fun getData(): Response {
-        val usersJsonObject = loadJsonFile(context)
+        val usersJsonObject = loadJsonFile(context).getJSONArray("data")
 
-        val userList = Gson().fromJson(usersJsonObject, Array<User>::class.java).toList()
+        val userList : List<User> = Gson().fromJson(usersJsonObject.toString(), Array<User>::class.java).toMutableList()
 
         return Response(
                 code = RESPONSE_CODE,
--- app/src/apiTesting/java/com/hasbihal/JsonUtil.kt
+++ app/src/apiTesting/java/com/hasbihal/JsonUtil.kt
@@ -7,12 +7,12 @@ import java.io.IOException
 
 
 @Throws(IOException::class, JSONException::class)
-fun loadJsonFile(context: Context): String {
+fun loadJsonFile(context: Context): JSONObject {
     val `is` = context.resources.openRawResource(R.raw.users)
     val size = `is`.available()
     val buffer = ByteArray(size)
     `is`.read(buffer)
     `is`.close()
-    //val jsonString = String(buffer)
-    return String(buffer)
+    val jsonString = String(buffer)
+    return JSONObject(jsonString)
 }
