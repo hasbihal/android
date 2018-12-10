dea01b76facbed9b1f790b7a7c50250ec5875600
Uğur Çakmak <ugurcmk@gmail.com>
2018-12-10 23:03:30 +0300

    Add divider item

--- app/src/main/java/com/hasbihal/main/UserListActivity.kt
+++ app/src/main/java/com/hasbihal/main/UserListActivity.kt
@@ -3,6 +3,8 @@ package com.hasbihal.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hasbihal.R
 import kotlinx.android.synthetic.main.activity_user_list.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -10,15 +12,18 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class UserListActivity : AppCompatActivity() {
 
     private val viewModel: UserViewModel by viewModel()
-    lateinit var adapter  : UserListAdapter
+    private val adapter by lazy { UserListAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_list)
 
+        lstMainOfUser.layoutManager = LinearLayoutManager(this)
+        lstMainOfUser.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        lstMainOfUser.adapter = adapter
+
         viewModel.loadUsers().observe(this, Observer {
-            adapter = UserListAdapter(applicationContext, it, R.layout.item_user_list)
-            lstMainOfUser.adapter = adapter
+            adapter.updateUsers(it)
         })
     }
 }
