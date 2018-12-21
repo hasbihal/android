1419b713ec7dba35be8c573dd9710bbfba2a7b2f
Uğur Çakmak <ugurcmk@gmail.com>
2018-12-21 23:09:24 +0300

    Refactor screen

--- app/src/main/java/com/hasbihal/chat/ChatActivity.kt
+++ app/src/main/java/com/hasbihal/chat/ChatActivity.kt
@@ -1,7 +1,8 @@
-package com.hasbihal
+package com.hasbihal.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hasbihal.R
 
 class ChatActivity : AppCompatActivity() {
 
