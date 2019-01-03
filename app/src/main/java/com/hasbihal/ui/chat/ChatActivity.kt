9a4dd668152b2cd429ab13d1567ca3e8bafff66e
Uğur Çakmak <ugurcmk@gmail.com>
2019-01-03 17:03:18 +0300

    Add dummy data

--- app/src/main/java/com/hasbihal/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/hasbihal/ui/chat/ChatActivity.kt
@@ -2,12 +2,38 @@ package com.hasbihal.ui.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hasbihal.R
+import kotlinx.android.synthetic.main.activity_chat.*
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var adapter: ChatAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        adapter = ChatAdapter()
+        adapter.setList( getDummyData() )
+
+        lstChat.layoutManager = LinearLayoutManager(this)
+        lstChat.adapter = adapter
+
+
+    }
+
+    private fun getDummyData(): List<Chat> {
+        var list = mutableListOf<Chat>()
+
+        list.add(Chat(1,"1","deneme 1",false))
+        list.add(Chat(1,"1","deneme 2",false))
+        list.add(Chat(1,"1","deneme 3",false))
+        list.add(Chat(1,"1","deneme 4",false))
+        list.add(Chat(1,"1","deneme 5",false))
+        list.add(Chat(1,"1","deneme 6",false))
+        list.add(Chat(1,"1","deneme 7",false))
+        list.add(Chat(1,"1","deneme 8",false))
+
+        return list
     }
 }
