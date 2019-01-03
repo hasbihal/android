e54b82bca9d41feca18d4e06d25a67961d2af79b
Uğur Çakmak <ugurcmk@gmail.com>
2019-01-03 15:42:13 +0300

    Add adapter for chat screen

--- app/src/main/java/com/hasbihal/ui/chat/ChatAdapter.kt
+++ app/src/main/java/com/hasbihal/ui/chat/ChatAdapter.kt
@@ -1,4 +1,18 @@
 package com.hasbihal.ui.chat
 
-class ChatAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatAdapter : RecyclerView.Adapter<ChatHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ChatHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
