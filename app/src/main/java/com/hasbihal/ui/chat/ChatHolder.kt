53b0b2b98d48893b666a4f50c9990d933e48c674
Uğur Çakmak <ugurcmk@gmail.com>
2019-01-03 17:04:33 +0300

    Implement chat view holder

--- app/src/main/java/com/hasbihal/ui/chat/ChatHolder.kt
+++ app/src/main/java/com/hasbihal/ui/chat/ChatHolder.kt
@@ -2,11 +2,11 @@ package com.hasbihal.ui.chat
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_chat_to.view.*
 
-class ChatHolder(view: View) : RecyclerView.ViewHolder(view) {
+class ChatHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-
-    fun bind(chat : Chat){
-
+    fun bind(item: Chat) = with(itemView) {
+        txtMessage.text = item.message
     }
 }
