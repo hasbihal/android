2b58643ff587d6d459b386342f6e05e6369e57b9
Uğur Çakmak <ugurcmk@gmail.com>
2019-01-03 16:32:09 +0300

    Implement chat adapter

--- app/src/main/java/com/hasbihal/ui/chat/ChatAdapter.kt
+++ app/src/main/java/com/hasbihal/ui/chat/ChatAdapter.kt
@@ -1,18 +1,29 @@
 package com.hasbihal.ui.chat
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.hasbihal.R
+import kotlinx.android.synthetic.main.item_chat_to.view.*
 
 class ChatAdapter : RecyclerView.Adapter<ChatHolder>() {
+
+    private lateinit var list: MutableList<Chat>
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ChatHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_chat_to, parent, false))
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return list.size
     }
 
     override fun onBindViewHolder(holder: ChatHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.itemView.txtMessage.text = list[position].message
+    }
+
+    fun setList(list: List<Chat>){
+        this.list.clear()
+        this.list.addAll(list)
     }
 }
