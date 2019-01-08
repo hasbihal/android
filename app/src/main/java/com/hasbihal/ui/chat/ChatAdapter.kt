fd7dede521e00cfd8e1a1db4d66270141f945178
Uğur Çakmak <ugurcmk@gmail.com>
2019-01-08 10:33:25 +0300

    Implement view type in order to determine the message's owner

--- app/src/main/java/com/hasbihal/ui/chat/ChatAdapter.kt
+++ app/src/main/java/com/hasbihal/ui/chat/ChatAdapter.kt
@@ -5,22 +5,36 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.hasbihal.R
 import kotlinx.android.synthetic.main.item_chat_to.view.*
+import timber.log.Timber
 
 class ChatAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val MESSAGE_FROM_ME = 99
+    private val MESSAGE_FROM_TO = 98
 
     private var list = mutableListOf<Chat>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return if(list[viewType].isMeSender){
-            ChatToHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_chat_to, parent, false))
-        }else{
-            ChatFromHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_chat_from, parent, false))
+        return when(viewType){
+            MESSAGE_FROM_TO -> ChatToHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_chat_to, parent, false))
+            MESSAGE_FROM_ME -> ChatFromHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_chat_from, parent, false))
+            else -> {
+                Timber.e("undefined view type")
+                ChatToHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_chat_to, parent, false))
+            }
         }
-
     }
 
     override fun getItemCount(): Int {
         return list.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if(list[position].isMeSender){
+            MESSAGE_FROM_ME
+        }else{
+            MESSAGE_FROM_TO
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
