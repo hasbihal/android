afb691708443ab089b2ed8ec6b168d64f190c583
Uğur Çakmak <ugurcmk@gmail.com>
2019-01-09 23:54:35 +0300

    Implement chat viewmodel

--- app/src/main/java/com/hasbihal/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/hasbihal/ui/chat/ChatActivity.kt
@@ -2,38 +2,30 @@ package com.hasbihal.ui.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hasbihal.R
 import kotlinx.android.synthetic.main.activity_chat.*
 
 class ChatActivity : AppCompatActivity() {
+
     private lateinit var adapter: ChatAdapter
+    private lateinit var viewModel: ChatViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
         adapter = ChatAdapter()
-        adapter.setList( getDummyData() )
+
+        viewModel = ViewModelProviders.of(this).get(ChatViewModel::class.java)
+        viewModel.getChats().observe(this, Observer {
+            adapter.setList(it)
+        })
 
         lstChat.layoutManager = LinearLayoutManager(this)
         lstChat.adapter = adapter
-
-
-    }
-
-    private fun getDummyData(): List<Chat> {
-        var list = mutableListOf<Chat>()
-
-        list.add(Chat(1,"1","deneme 1",false))
-        list.add(Chat(1,"1","deneme 2",true))
-        list.add(Chat(1,"1","deneme 3",false))
-        list.add(Chat(1,"1","deneme 4",true))
-        list.add(Chat(1,"1","deneme 5",false))
-        list.add(Chat(1,"1","deneme 6",true))
-        list.add(Chat(1,"1","deneme 7",false))
-        list.add(Chat(1,"1","deneme 8",true))
-
-        return list
     }
 }
--- app/src/main/java/com/hasbihal/ui/chat/ChatViewModel.kt
+++ app/src/main/java/com/hasbihal/ui/chat/ChatViewModel.kt
@@ -1,4 +1,27 @@
 package com.hasbihal.ui.chat
 
-class ChatViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ChatViewModel: ViewModel() {
+
+    private val chats = MutableLiveData<List<Chat>>()
+
+    fun getChats() : LiveData<List<Chat>> {
+        val list = ArrayList<Chat>()
+
+        list.add(Chat(1,"1","deneme 1",false))
+        list.add(Chat(1,"1","deneme 2",true))
+        list.add(Chat(1,"1","deneme 3",false))
+        list.add(Chat(1,"1","deneme 4",true))
+        list.add(Chat(1,"1","deneme 5",false))
+        list.add(Chat(1,"1","deneme 6",true))
+        list.add(Chat(1,"1","deneme 7",false))
+        list.add(Chat(1,"1","deneme 8",true))
+
+        chats.value = list
+
+        return chats
+    }
 }
