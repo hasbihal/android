5c2de90922040f8f71da9a2cabf0f3529fec0aa7
Uğur Çakmak <ugurcmk@gmail.com>
2019-01-23 17:00:04 +0300

    Initialize Firebase

--- app/src/main/java/com/hasbihal/ui/login/LoginActivity.kt
+++ app/src/main/java/com/hasbihal/ui/login/LoginActivity.kt
@@ -2,12 +2,27 @@ package com.hasbihal.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.hasbihal.R
+import timber.log.Timber
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var fireBaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        fireBaseAuth = FirebaseAuth.getInstance()
+
+        fireBaseAuth.createUserWithEmailAndPassword("ugurcmk@gmail.com", "12345678")
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Timber.d("The task is successfully completed")
+                    } else {
+                        Timber.d("There is an error : "+task.exception)
+                    }
+                }
     }
 }
